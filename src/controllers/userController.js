02b45e8b3167d71ec8b6a8dc12cb1dd265c3a82f
mycalvo <122553621+mycalvo@users.noreply.github.com>
2024-10-08 17:28:26 +0200

    Update and rename User Controller.php to userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,68 @@
+const User = require('../models/user');
+
+const getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      await user.update(req.body);
+      res.json(user);
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      await user.destroy();
+      res.status(204).send();
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+module.exports = {
+  getUserInfo,
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser
+};
