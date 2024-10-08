517c63f4a128c0c1f9936c2f004c6330486fbfcf
mycalvo <122553621+mycalvo@users.noreply.github.com>
2024-10-08 17:52:17 +0200

    Update index.js

--- public/index.js
+++ public/index.js
@@ -8,14 +8,26 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+// Rutas de API
 app.get('/user/:id', userController.getUserInfo);
 app.get('/users', userController.getAllUsers);
 app.post('/user', userController.createUser);
 app.put('/user/:id', userController.updateUser);
 app.delete('/user/:id', userController.deleteUser);
 
+// Ruta para el panel de administración
 app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/views/admin.html'));
+});
+
+// Ruta para la raíz
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/views/admin.html'));
+});
+
+// Ruta para el archivo estático index.html
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/views/index.html'));
 });
 
 app.listen(port, () => {
